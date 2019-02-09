0a2eb01723ea32e191aad6e9ccab18e314c432cd
unknown <ardvanbalkom@gmail.com>
2019-02-09 16:16:42 +0000

    Added some lines to make sure user has to enter a positive integer

--- PythonProblem1.py
+++ PythonProblem1.py
@@ -1,7 +1,7 @@
 # Ard van Balkom
 # Problem 1, enter a positive integer and calculate the sum of all numbers between 1 and the integer entered
 
-i = input( " Please enter an integer: " )
+i = input( " Please enter a positive integer: " )
 
 i = int( i )
 
@@ -9,5 +9,8 @@ x = sum (range(1, i + 1))
 
 x = int( x )
 
-print ( " The sum of all numbers between 1 and the integer you entered is" , x)
+if i>0:
+    print ( " The sum of all numbers between 1 and the integer you entered is" , x)
+else:
+    print ( " That is not a positive integer ")
 
