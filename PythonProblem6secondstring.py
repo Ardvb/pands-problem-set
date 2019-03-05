7232ccf8c9805f403251982624e2ecf5c9bc6441
unknown <ardvanbalkom@gmail.com>
2019-03-05 03:34:26 +0000

    trying to figure out how to take away every other word. No luck yet.

--- PythonProblem6secondstring.py
+++ PythonProblem6secondstring.py
@@ -1,5 +1,5 @@
 # This program takes a user input string and outputs every second word.
-# first attempt
+# first attempt. Can't figure out how to program it so it takes away every other word.
 
 sentence = str(input("Please enter a sentence: "))
 
