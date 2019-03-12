edffc0ca6090784e83ef8859059af88156fb79f9
unknown <ardvanbalkom@gmail.com>
2019-03-12 19:21:26 +0000

    problem 8 working, trying to code more efficient though

--- PythonProblem8datetime.py
+++ PythonProblem8datetime.py
@@ -6,10 +6,17 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-datum = datetime.now()
+datum = datetime.now() #created variable "datum" that will represent the exact date and time when the program is run.
+year = datum.year 
+day = datum.day
+hour = datum.hour
+minute = datum.minute
+add0 = "" #added this variable that changes to 0 if it is less than 10 minutes past the hour, to display 5 past 7, as 7:05, instead of 7:5.
 
-if date.today().weekday() == 0: #assigning the names of the days to every weekday. Trying to figure out an easier and prettier way to do this.....
-    weekday = "Monday"
+
+
+if date.today().weekday() == 0: #assigning the names of the days to every weekday. (There probably is a module for this, but I prefer to do it this way for now.)
+    weekday = "Monday" # This way I can see exactly what I am doing and I learn more doing it that way.
 elif date.today().weekday() == 1:
     weekday = "Tuesday"
 elif date.today().weekday() == 2:
@@ -24,7 +31,7 @@ elif date.today().weekday() == 6:
     weekdday = "Sunday"
 
 
-if datum.month == 1:
+if datum.month == 1: #assigning the names of the months to each months' number. (There probably is a module for this as well)
     month = "January"
 elif datum.month == 2:
     month = "February"
@@ -49,13 +56,23 @@ elif datum.month == 11:
 elif datum.month == 12:
     month = "December"
 
-if datum.day >= 3: #it works, but doesnt look pretty at all. Will have to figure out another way of doing it.
-    day = str((datum.day, "th"))
+if datum.day >= 3: #adding the correct 'addon' to the date. If it is the 1st or the 2nd this will be different from the other dates.
+    addon = "th" #variable 'addon' will display the correct addon to the date.
 elif datum.day == 2:
-    day = str((datum.day, "nd"))
+    addon = "nd"
 else:
-    day = str((datum.day, "st"))
-
-print(weekday, month, day)
+    addon = "st"
 
 
+if hour >= 12: #adding the correct 12-hour indication to the hours and minutes (am or pm). After 12 noon this will show 'pm', otherwise it will show 'am'.
+    pmam = "pm"   
+else: 
+    pmam = "am"
+
+if hour >= 13: #by subtracting 12 from all hours 13 or higher, all hours apart from 12, will be shown with 1 digit.
+    hour = hour-12
+
+if minute <10: #if it's less than 10 minutes past the hour, the variable 'add0' will become 0.
+    add0 = "0"
+
+print("Today's date and time is {}, {} {}{} {} at {}:{}{}{} ".format( weekday, month, day, addon, year, hour, add0, minute, pmam )) #"Today's date and time is Tuesday, March 12th 2019 at 7:20pm"
