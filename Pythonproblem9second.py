87c8d5b8197a412374d888dae145cef0cb7ddb91
unknown <ardvanbalkom@gmail.com>
2019-03-13 01:26:51 +0000

    figured out how to display every second line

--- Pythonproblem9second.py
+++ Pythonproblem9second.py
@@ -1,8 +1,12 @@
-n = input("Please enter the filename of the textfile you would like to open: ") #Let the user input a textfile he or she would like to open
+#This program reads a textfile of the user's choice and displays every second line.
 
-text = open(n, 'r') #open the textfile 'Gedicht' from the same folder as this program
+text = input("Please enter the filename of the textfile you would like to open: ") #Let the user input a textfile he or she would like to open
 
-for line in text:
-    print(line, end=" ") #prints all lines from the file the user has opened. I have to figure out next how to display only the second line
+with open(text, "r") as f: #from now on the file that the user has opened and is read by the program, will be called 'f'.
+    n = 0 #I will use variable n as a count, to count all the lines in the textfile.
+    for line in f:
+        n += 1 #increases the count by 1 every time the loop is repeated. 
+        if n%2 == 0: #using the module operator, I only include the even lines (eg. second, fourth, etc.)
+            print(line) #prints all even lines from the textfile the user has opened.
 
 
