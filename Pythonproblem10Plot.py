160d11077103d32e9cede55df96406525130bb8e
unknown <ardvanbalkom@gmail.com>
2019-03-13 02:39:19 +0000

    first attempt at problem 10, yet to figure out how to display a plot

---
+++ Pythonproblem10Plot.py
@@ -0,0 +1,9 @@
+# This program will display a plot of the functions x, x**x and 2**x
+
+# I will have to import a module for this, but I have to figure out which one.
+
+
+for x in range (1,4):
+    a = x**x
+    b = 2**x
+    print (a, b, x) #this prints the results. I have to figure out now how to display a plot.
