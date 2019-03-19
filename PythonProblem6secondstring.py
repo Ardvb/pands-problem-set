051f92da572a7b8cc76fc52889f336806b60b55b
unknown <ardvanbalkom@gmail.com>
2019-03-19 17:25:18 +0000

    figured out how to plot the functions importing matplotlib

--- PythonProblem6secondstring.py
+++ PythonProblem6secondstring.py
@@ -3,7 +3,7 @@
 
 sentence = str(input("Please enter a sentence: "))
 
-wordlist = sentence.split() # This splits the entered sentence into seperate words, using the blank space to recognize the seperate words, and makes it into a list.
+wordlist = sentence.split() # This splits the entered sentence into separate words, using the blank space to recognize the seperate words, and makes it into a list.
 length = (len(wordlist)) # this counts the total amount of words in the sentence
 half = len(wordlist) //2 #the variable 'half' is needed to make the while loop stop on time.
 x = -1 #this removes the last word from the string
@@ -15,7 +15,7 @@ while length > half: #the loop will stop when half of all words are removed.
     x = x - 1 #after removing the last word, x becomes -2, so it removes the second last word.
     #  (after the last one is already gone, so from the string that was entered, the last and third last word are gone etc.)
     
-joinedstring = ' '.join([str(word) for word in wordlist]) #Make the list of words into a string again and then join them with a single space in between every seperate word.
+joinedstring = ' '.join([str(word) for word in wordlist]) #Make the list of words into a string again and then join them with a single space in between every separate word.
     
 print(joinedstring) #print the new joined string
 
--- Pythonproblem10Plot.py
+++ Pythonproblem10Plot.py
@@ -1,9 +1,25 @@
-# This program will display a plot of the functions x, x**x and 2**x
-
-# I will have to import a module for this, but I have to figure out which one.
+# This program will display a plot of the functions x, x*x and 2**x, in the range[0,4]
 
 
-for x in range (1,4):
-    a = x**x
-    b = 2**x
-    print (a, b, x) #this prints the results. I have to figure out now how to display a plot.
+import matplotlib.pyplot as plt #import matplotlib
+import numpy as np #import Numpy for maths and working with arrays
+
+x = np.linspace(0,4,5) #set the min(0) and max(4) values for the x axis, and the number(5) of equally spaced samples.
+
+
+y1 = x #these are the 3 functions that will be plotted.
+y2 = x**2
+y3 = 2**x
+
+plt.plot(y1, label="x") #plot y1 and label it "x"
+plt.plot(y2, label="x square") #plot y2 and label it "x square"
+plt.plot(y3, label="2**x") #plot y2 and label it "2**x"
+    
+plt.xlabel("X") #label the X-axis "X"
+plt.ylabel("Y") #label the Y-axis "Y"
+    
+plt.title("Functions of x in range [0,4]") #give the plot a title
+
+plt.legend() #show the labels above the plot, so it is clear what each of the coloured lines represent.
+
+plt.show() #show the plot
