50aa3cf1ead88c27f07e281d518296c7c084fcc6
unknown <ardvanbalkom@gmail.com>
2019-03-19 17:41:01 +0000

    added some comments

--- Pythonproblem10Plot.py
+++ Pythonproblem10Plot.py
@@ -1,11 +1,12 @@
 # This program will display a plot of the functions x, x*x and 2**x, in the range[0,4]
 
 
+
 import matplotlib.pyplot as plt #import matplotlib
 import numpy as np #import Numpy for maths and working with arrays
 
 x = np.linspace(0,4,5) #set the min(0) and max(4) values for the x axis, and the number(5) of equally spaced samples.
-
+#I chose to connect to points for x = 0, x = 1, x = 2, x = 3 and x = 4.
 
 y1 = x #these are the 3 functions that will be plotted.
 y2 = x**2
