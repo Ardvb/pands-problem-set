acd3df72c944e6ada91bfd903f53d8c8ec12fe32
unknown <ardvanbalkom@gmail.com>
2019-03-19 19:04:13 +0000

    added some comments

--- PythonProblem2.py
+++ PythonProblem2.py
@@ -7,9 +7,10 @@ import datetime #First we have to import the module 'datetime' and 'date' so tha
 from datetime import date
 
 
-if date.today().weekday() == 1 or date.today().weekday() == 3: #Since we start counting at 0, Tuesday is day 1 and Thursday is day 3. Check if today is Tuesday or Thursday.
-    print ( "Yes, today begins with a T" )
+if date.today().weekday() == 1 or date.today().weekday() == 3: #by using the weekday function (where Monday is 0 and Sunday 6, our program can determine what day of the week it is.
+    #Since we start counting at 0, Tuesday is day 1 and Thursday is day 3. Check if today is Tuesday or Thursday.
+    print ( "Yes, today begins with a T" ) #display a message saying today starts with a T
 
 
 else: #If it turned out not to be Tuesday or Thursday, then we display the following message:
-    print ("No, today doesn't begin with a T")
+    print ("No, today doesn't begin with a T") #display a message saying today doesn't begin with a T
