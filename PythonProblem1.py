b78b8680553617d61e15dd872f05b427244b263b
unknown <ardvanbalkom@gmail.com>
2019-03-19 18:52:13 +0000

    fixed pr

--- PythonProblem1.py
+++ PythonProblem1.py
@@ -6,7 +6,7 @@ while True: # only run the rest of the program if the user enters an integer
     try:
         i = int(input(" Please enter a positive integer: ")) # ask the user to enter a positive integer.
     except ValueError:
-        print("That is not an integer, try again")
+        print(" That is not an integer, try again")
         continue 
 
     x = sum (range(1, i + 1)) # a formula that gives the user the sum of all numbers between 1 and the integer entered (including the entered integer, hence the +1 at the end)
@@ -16,7 +16,7 @@ while True: # only run the rest of the program if the user enters an integer
 
     if i>0: # only run the calculation if the input was a positive integer
         print (" The sum of all numbers between 1 and the integer you entered is" , x) # display the sum of all numbers between 1 and the entered integer.
-
+        break
     else:
-        print (" That is not a positive integer ") # if user did not enter a positive integer, display a message saying what went wrong.
-
+        print (" That is not a positive integer, try again ") # if user did not enter a positive integer, display a message saying what went wrong.
+        
