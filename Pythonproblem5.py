3b556a2f10c93c65d14c59031760f6f313cba3da
unknown <ardvanbalkom@gmail.com>
2019-03-21 20:08:02 +0000

    Final Version Problem 5

--- Pythonproblem5.py
+++ Pythonproblem5.py
@@ -7,11 +7,11 @@ while True: # Run a while loop that will run indefinitely.
         try: # Test the next block of code for errors
                 num = int( input("enter a positive integer: ")) # Ask user to enter an integer. This input will be 'num'.
         except ValueError: # Don't run the rest of the program if the user makes a ValueError (enters a non-integer), instead run the following print command.
-                print(" That is not a positive integer, please try again") # If a non numeric value is entered, a message will be shown and program is ran again.
+                print(" That is not an integer, please try again") # If a non numeric value is entered, a message will be shown and program is ran again.
                 continue # Return to the beginning of the loop, so user can try again to enter an integer.
-        if num <= 0:
-                print(" That is not a positive integer, please try again") #if a number smaller or equal to 0 has been entered, a message will be shown and program is ran again.
-                continue 
+        if num <= 0:  # If a number smaller or equal to 0 has been entered, a message will be shown and program is ran again.
+                print(" That is not a positive integer, please try again")
+                continue # Return to the beginning of the loop, so user can try again to enter an integer.
                 # Adapted from: https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
                 # Used page 82 of http://spronck.net/pythonbook/pythonbook.pdf to learn about the infinite while loop.
         count = 0 # This variable starts at 0 and will be used to count up and check every number up to the user input 'num'.
@@ -27,9 +27,11 @@ while True: # Run a while loop that will run indefinitely.
                 print(" This is not a prime number ")
         else:
                 print(" This is a prime number ")
-        break #stop running the program if it has been printed if the number is either a prime number or not.
+        break # Stop running the program if it has been printed if the number is either a prime number or not.
 # If the loop is completed until it reaches 'num', with a 'total' of 2 or lower, this means the number entered is not divisible by any numbers, other than 1 or itself. 
 # Therefore we can display "this is a prime number".
  
 
+
+# I later realized I could have started the loop at 2 and let it run until the entered number, which would have been a slightly easier code that would have worked.
 
