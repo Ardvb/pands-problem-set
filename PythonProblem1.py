a4396d7ca560a5cbfc2f8b4aab31eb014d59cb9b
unknown <ardvanbalkom@gmail.com>
2019-03-21 18:15:46 +0000

    final version problem 1

--- PythonProblem1.py
+++ PythonProblem1.py
@@ -1,20 +1,20 @@
-# Ard van Balkom
-# Problem 1, enter a positive integer and calculate the sum of all numbers between 1 and the integer entered
+# Ard van Balkom, 7-2-19
+# Problem 1, enter a positive integer and calculate the sum of all numbers between 1 and the integer entered.
 
-while True: # Only run the rest of the program if the user enters an integer
-    # Otherwise display a message and start program again.
-    # Adapted from: https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
-    try:
+# The code to check if user enters an integer, using 'try' and 'except' is adapted from: https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
+
+while True: # Run a while loop that will run indefinitely.
+    try: # Test the next block of code for errors
         i = int(input(" Please enter a positive integer: ")) # Ask the user to enter a positive integer.
-    except ValueError:
-        print(" That is not an integer, try again") # Show message if user inputs something other than an integer.
+    except ValueError:  # Don't run the rest of the program if the user makes a ValueError (enters a non-integer), instead run the following print command.
+        print(" That is not an integer, try again")
         continue # Return to the beginning of the loop, so user can try again to enter an integer.
 
-    x = sum (range(1, i + 1)) # A formula that gives the user the sum of all numbers between 1 and the integer entered (including the entered integer, hence the +1 at the end)
+    x = sum (range(1, i + 1)) # Calculate the sum of all numbers betweem 1, and the integer entered (including the entered integer, hence the +1 at the end).
 
     if i>0: # Only run the calculation if the input was a positive integer
         print (" The sum of all numbers between 1 and the integer you entered is" , x) # Display the sum of all numbers between 1 and the entered integer.
-        break
+        break # End the indefinite while loop and the program.
     else: 
-        print (" That is not a positive integer, try again ") # If user did not enter a positive integer, display a message saying what went wrong.
+        print (" That is not a positive integer, try again ") # If user entered a negative number, display a message asking user to try again.
         
