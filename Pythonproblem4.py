4ac593fcc96168633d8cfd3c8411a4d40ecad0c5
unknown <ardvanbalkom@gmail.com>
2019-03-21 19:51:14 +0000

    Final Version of Problem 4

--- Pythonproblem4.py
+++ Pythonproblem4.py
@@ -1,17 +1,19 @@
-# Ard van Balkom
-# Asking user for a positive integer. then output the successive value.
+# Ard van Balkom, 9-2-19
+# Problem 4. Asking user for a positive integer. then output the successive value.
 # If current value is even, then it is divided by 2. If current value is odd, it is multiplied by 3 and 1 is added. If current value is 1, the program ends.
 
-while True:
-    try:
+while True: # Run a while loop that will run indefinitely.
+    try: # Test the next block of code for errors
         num = int( input("Please enter a positive integer: ")) # Ask the user to input a positive integer. The input from the user is the variable 'num'.
-    except ValueError:
-        print("That is not a positive integer, try again") # If the user enters a non numeric input, a message will show and program runs again.
-        continue
-    if num <= 0:
-        print("That is not a positive integer, try again") # If the user enters a number that is 0 or lower than 0, a message will show and program runs again.
-        continue  # Adapted from: https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
-    total = 0 # Create a variable called 'total', that starts at 0.
+    except ValueError: # Don't run the rest of the program if the user makes a ValueError (enters a non-integer), instead run the following print command.
+        print("That is not an integer, try again") # If the user enters a non numeric input, a message will show and program runs again.
+        continue # Return to the beginning of the loop, so user can try again to enter an integer.
+    if num <= 0: # If the user enters a number that is 0 or lower than 0, a message will show and program runs again.
+        print("That is not a positive integer, try again") 
+        continue  # Return to the beginning of the loop, so user can try again to enter an integer.
+        # Adapted from: https://stackoverflow.com/questions/23326099/how-can-i-limit-the-user-input-to-only-integers-in-python
+        # Used page 82 of http://spronck.net/pythonbook/pythonbook.pdf for the infinite while loop.
+    total = 0 # Create a variable 'total', starting at 0, that will be calculated after user enters 'num' and will be printed.
 
 
     while num != 1 and num >= 0: # Keep running the loop until num == 1
