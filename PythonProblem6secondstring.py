c99ca3d6f19fb0ca76c19aff6c1020762a2185c9
unknown <ardvanbalkom@gmail.com>
2019-03-22 00:00:44 +0000

    Final Version Problem 6

--- PythonProblem6secondstring.py
+++ PythonProblem6secondstring.py
@@ -1,17 +1,17 @@
-# Ard van Balkom
-# This program takes a user input string and outputs every second word.
+# Ard van Balkom, 5-3-19
+# Problem 6. This program takes a user input string and outputs every second word.
 
 
-sentence = str(input("Please enter a sentence: ")) # Asks user to enter a string
+sentence = str(input("Please enter a sentence: ")) # Ask user to enter a string
 
-wordlist = sentence.split() # This splits the entered sentence into separate words, using the blank space to recognize the seperate words, and makes it into a list.
+wordlist = sentence.split() # Split the entered sentence into separate words, using the blank space to recognize the seperate words, and makes it into a list.
 n = 0 # Sets 'n' as variable which will be used as a count.
 for word in wordlist:
-    n+=1 # Adds 1 to n, so all words in the wordlist are considered for the criteria 'if n != 0', which then prints only all odd words, and removes every second (even) word.
+    n+=1 # Adds 1 to n, so all words in wordlist are considered, ony by one, for the criteria 'if n != 0', which then prints only all odd words, and removes every second (even) word.
     if n%2 != 0:
         print(word, end=' ') # Print all odd words, without going to a new line after every word, with a space between them.
 
-
+# The code to split the sentence into a list of words is adapted from https://stackoverflow.com/questions/743806/how-to-split-a-string-into-a-list
 
 
 
